27a34e9a993e63c6b4aa18f009d10387eb73633f
Nosang Subba <nosangdev@gmail.com>
2024-11-14 17:31:28 +0530

    added hero page

---
+++ app/componets/hero/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./styles.css"
+import Image from 'next/image'
+type Props = {}
+
+export default function Hero({ }: Props) {
+  return (
+    <section className='h-screen w-full max-sm:px-4 hero overflow-hidden'>
+      <nav className='flex md:px-5 py-2 '>
+        <h1 className='md:text-4xl text-2xl text-[#1a0e4f] font-black font-sans'>संगठन</h1>
+      </nav>
+      <div className='h-[calc(100vh-100px)] flex justify-center items-center flex-col'>
+        <div className='flex flex-col justify-center items-center md:gap-y-3'>
+          <h2 className='md:text-7xl text-2xl text-center font-sans md:font-bebas-neue hero-text font-extrabold'>
+            From Local Gatherings to Big Events</h2>
+          <h2 className='md:text-7xl text-2xl text-center font-sans md:font-bebas-neue hero-text font-extrabold'>
+            Find Your Next Experience
+          </h2>
+        </div>
+        <div className=' flex justify-center max-md:flex-col items-center mt-4 md:mt-[2rem] gap-1 md:gap-4'>
+          <button className='cta-btn max-md:w-full font-sans max-md:!p-2 md:!px-3 max-md:!text-xs hover:bg-[#fafafa] transition-colors hover:text-[#1a1a1a]'>
+            Explore Meetups
+          </button>
+          <button className='cta-btn max-md:w-full font-sans max-md:!p-2 md:!px-3 max-md:!text-xs cta-outline transition-colors'>
+            Start Hosting
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import { Bebas_Neue } from "next/font/google"
+import "./globals.css"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,6 +13,12 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400", // Specify the weight available
+  variable: "--font-bebas-neue" // Optional, for CSS variable
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${bebasNeue.variable} antialiased`}
       >
         {children}
       </body>
